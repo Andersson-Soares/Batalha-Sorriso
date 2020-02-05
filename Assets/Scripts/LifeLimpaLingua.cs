d07cdac924779f675c6b48527418b2c6e70c245f
Andersson Soares <anderssonsoares97@gmail.com>
2020-02-05 15:43:43 -0300

    Botão Tutorial

---
+++ Assets/Scripts/LifeLimpaLingua.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeLimpaLingua : MonoBehaviour
+{
+    
+    void OnCollisionEnter2D(Collision2D outros)
+    {
+        if(outros.gameObject.CompareTag("Carie"))
+        {
+            transform.Translate (new Vector3(0.5f,0,0));
+
+            Destroy(outros.gameObject);
+        }
+
+    }
+}
