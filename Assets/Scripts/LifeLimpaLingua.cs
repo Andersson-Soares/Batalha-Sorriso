04737448baa8703d6dfdbec30680ff313ef3053d
Andersson Soares <anderssonsoares97@gmail.com>
2020-02-06 19:55:44 -0300

    Tentando Construir o Ataque do LimpaLingua

--- Assets/Scripts/LifeLimpaLingua.cs
+++ Assets/Scripts/LifeLimpaLingua.cs
@@ -4,14 +4,12 @@ using UnityEngine;
 
 public class LifeLimpaLingua : MonoBehaviour
 {
-    
+
     void OnCollisionEnter2D(Collision2D outros)
     {
         if(outros.gameObject.CompareTag("Carie"))
         {
-            transform.Translate (new Vector3(0.5f,0,0));
-
-            Destroy(outros.gameObject);
+                Destroy(outros.gameObject);
         }
 
     }
