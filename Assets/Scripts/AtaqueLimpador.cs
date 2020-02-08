91791fa24484ddb1f54b28817299b0ae1b1c8052
Andersson Soares <anderssonsoares97@gmail.com>
2020-02-08 15:28:18 -0300

    Excluindo Arquivos Mortos

--- Assets/Scripts/AtaqueLimpador.cs
+++ Assets/Scripts/AtaqueLimpador.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LifeLimpaLingua : MonoBehaviour
+public class AtaqueLimpador : MonoBehaviour
 {
-
+   
     void OnCollisionEnter2D(Collision2D outros)
     {
         if(outros.gameObject.CompareTag("Carie"))
         {
+           transform.position = Vector3.Lerp (transform.position,new Vector3 (5,5,5), Time.deltaTime);
                 Destroy(outros.gameObject);
         }
 
