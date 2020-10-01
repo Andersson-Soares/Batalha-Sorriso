dc447c3b725f742210a97d1c3f2fb1cf6f5012f7
Andersson Soares <anderssonsoares97@gmail.com>
2020-10-01 14:09:14 -0300

    Refinando as Fases

--- Assets/Scripts/CremeColetor.cs
+++ Assets/Scripts/CremeColetor.cs
@@ -6,10 +6,12 @@ public class CremeColetor : MonoBehaviour {
 
 	public static int cremeDisponivel = 100;
 
+
 	void OnMouseDown(){
 		cremeDisponivel += 25;
         print(cremeDisponivel);
 		GerenciaAudio.instance.SonsFXToca (0);
 		Destroy (gameObject);
 	}
+
 }
--- Assets/Scripts/CremeSpawn.cs
+++ Assets/Scripts/CremeSpawn.cs
@@ -11,11 +11,13 @@ public class CremeSpawn : MonoBehaviour
     void Start()
     {
         InvokeRepeating("Spawn", tempoSpawn, tempoSpawn);
+
     }
 
     public void Spawn()
     {
         Vector3 aux = new Vector3(transform.position.x + Random.Range(-0.5f,0.5f), transform.position.y + Random.Range(-0.5f, 0.5f), transform.position.z - 1);
         Instantiate(prefab, aux, Quaternion.identity);
+
     }
 }
